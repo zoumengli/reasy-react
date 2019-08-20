ff99ef68255c46550002c17734c87691b799265f
zoumengli <1554525813@qq.com>
2019-08-20 13:08:22 +0800

    first commit

---
+++ src/Main.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { App } from './App';
+
+export interface IMainProps
+{
+    app: App; // Reference to our App.ts class
+}
+
+export class Main extends React.Component<IMainProps, {}>
+{
+    constructor(props: IMainProps)
+    {
+        super(props);
+    }
+
+    public render(): JSX.Element
+    {
+        return (
+            <>
+            Main app
+            </>
+        );
+    }
+}
